28be62becc06f5f5277e4ddf91f1c3dea00f8e28
camargo <camargo.cac@gmail.com>
2016-01-07 14:52:25 -0800

    remove unused import

--- app/contact-list.component.ts
+++ app/contact-list.component.ts
@@ -2,11 +2,9 @@ import {Component, OnInit} from 'angular2/core';
 import {Router} from 'angular2/router';
 import {Contact} from './contact';
 import {ElasticApiService} from './elastic-api.service';
-import {ContactDetailComponent} from './contact-detail.component';
 
 @Component({
     selector: 'contact-list',
-    directives: [ContactDetailComponent],
     providers: [ElasticApiService],
     template: `
         <table>
