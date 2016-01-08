c75d8ff1e77af9db6c8ff2b625cbbe6fbfad035e
Chris <camargo.cac@gmail.com>
2016-01-07 21:41:15 -0800

    Update boot.ts

--- app/boot.ts
+++ app/boot.ts
@@ -1,7 +1,8 @@
+// Info about boot.ts: https://angular.io/docs/ts/latest/quickstart.html#!#boot
+
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {CrudAppComponent} from './crud-app.component';
 
-// Info: https://angular.io/docs/ts/latest/quickstart.html#!#boot
 bootstrap(CrudAppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
